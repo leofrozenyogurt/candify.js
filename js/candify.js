1b06c44e63ee60bd3d3b942ce9b4d48fd824e34c
Leo Royzengurt <rebellabs@gmail.com>
2013-11-02 20:52:05 -0400

    more candify features

--- js/candify.js
+++ js/candify.js
@@ -5,14 +5,16 @@
         // This is the easiest way to have default options.
         var settings = $.extend({
             // These are the defaults.
-            color: "yellow",
-            backgroundColor: "transparent"
+            color: "black",
+            backgroundColor: "white",
+            borderColor:"grey"
         }, options );
  
         // Candify the collection based on the settings variable.
         return this.css({
             color: settings.color,
-            backgroundColor: settings.backgroundColor
+            backgroundColor: settings.backgroundColor,
+            boxShadow: ('0px 3px 0px 0px '+ settings.borderColor)
         }).addClass('candify');
  
     };
